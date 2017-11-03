ff423c7b9b2b434abc19e39810c5ecdfd4909107
Yamuna Navada <yamunanavada@gmail.com>
2017-11-03 01:56:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
 
+  if block_given?
+    i = 0
+    new_arr = []
+    while i < array.length
+      new_arr.push(yield array[i])
+      i = i+1
+    end
+  end
 
+new_arr
+
+end
